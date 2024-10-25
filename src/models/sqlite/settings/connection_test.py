ee05ebe0ce1156603c3e71eda9f7c06157170560
Ruy Freire <ruy.slv@gmail.com>
2024-10-25 10:30:47 -0300

    iniciando repo

---
+++ src/models/sqlite/settings/connection_test.py
@@ -0,0 +1,12 @@
+from sqlalchemy.engine import Engine
+from src.models.sqlite.settings.connection import db_connection_handler
+
+
+def test_connect_to_db():
+    assert db_connection_handler.get_engine() is None
+
+    db_connection_handler.connect_to_db()
+    db_engine = db_connection_handler.get_engine()
+
+    assert db_engine is not None
+    assert isinstance(db_engine, Engine)
